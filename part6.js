b328fc8244ba47bd35bc871729317832dec98e2e
nimishawilson <nimishawilson123@gmail.com>
2019-09-21 19:19:32 +0530

    template literals, destructuring, getters and setters

---
+++ part6.js
@@ -0,0 +1,8 @@
+//understand the difference between import and require
+//import { capitalizeString, foo } from  "./string_function";
+var express = require("./string_function");
+const cap = capitalizeString("hello");
+ console.log(cap);
+
+ console.log(foo);
+
---
+++ string_function.js
@@ -0,0 +1,4 @@
+export const capitalizeString = str => str.toUpperCase();
+
+ //use export to reuse a code block
+ export const foo = "hai";
